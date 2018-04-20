b2766d5031ceb9584028335b3509100d6b42b75f
Megan Williamson <meganawilliamson@gmail.com>
2018-04-20 10:35:19 -0700

    login functionality complete

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -5,6 +5,7 @@ const User = require('../models/userModels');
 const login = (req, res) => {
   const { username, password } = req.body;
   User.findOne({ username }, (err, user) => {
+    console.log(err, user);
     if (err) {
       res.status(403).json({ error: 'Invalid Username/Password' });
       return;
@@ -21,7 +22,7 @@ const login = (req, res) => {
       }
       if (hashMatch) {
         const payload = {
-          username: user.username
+          username: user.username,
         }; // what will determine our payload.
         const token = jwt.sign(payload, mysecret); // creates our JWT with a secret and a payload and a hash.
         res.json({ token }); // sends the token back to the client
@@ -31,5 +32,5 @@ const login = (req, res) => {
 };
 
 module.exports = {
-  login
+  login,
 };
